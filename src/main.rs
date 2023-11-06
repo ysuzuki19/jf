bd4f201d27ce7ab0cffa4767e5a79fff2db1b9dc
ysuzuki19 <ysuzuki.cpp@gmail.com>
2023-11-07 02:27:24 +0900

    update command

--- src/main.rs
+++ src/main.rs
@@ -20,10 +20,10 @@ async fn cli() -> CmdResult<()> {
     commander.run("greet-slow".into()).await?;
     // commander.run("greet-parallel".into()).await?;
     // commander.run("greet-watch").await?;
-    commander.run("incrementing".into()).await?;
-    // commander.run("incrementing-watch".into()).await?;
+    commander.run("incl10".into()).await?;
+    // commander.run("incl10-watch".into()).await?;
     // commander.run("sequential-echos-watch".into()).await?;
-    // commander.run("incrementing-sequential".into()).await?;
+    // commander.run("incl10-sequential".into()).await?;
     // commander.run("run".into()).await?;
     // commander.run("live-run".into()).await?;
 
