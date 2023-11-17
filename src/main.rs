a432178dd36ad9f62c5b3884a8bd33769498b999
ysuzuki19 <42496938+ysuzuki19@users.noreply.github.com>
2023-11-18 05:50:12 +0900

    impl cmd tool

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,64 @@ mod error;
 mod task;
 mod taskdef;
 
+use clap::Parser;
+
 use error::CmdResult;
 
-async fn cli() -> CmdResult<()> {
+#[derive(Parser, Debug)]
+#[command(author = "ysuzuki19", version, about, long_about = None)]
+#[command(disable_help_flag = true)]
+struct Args {
+    #[command(subcommand)]
+    sub_command: Option<SubCommand>,
+}
+
+#[derive(Parser, Debug, Clone)]
+enum SubCommand {
+    // Completion {
+    //     shell: Shell,
+    // },
+    #[command(about = "Run a task")]
+    Run { task_name: String },
+
+    #[command(about = "Description a task")]
+    Description { task_name: String },
+}
+
+async fn cli(args: Args) -> CmdResult<()> {
     let config_file = "cmdrc.toml";
     let config_contents = std::fs::read_to_string(config_file)?;
     let config: config::CmdConfig = toml::from_str(&config_contents)?;
 
     let commander = commander::Commander::new(config)?;
-    // let task_name = "incl10-parallel-watch".to_string();
-    let task_name = "sequential-echos-watch".to_string();
-    commander.description(task_name.clone())?;
-    commander.run(task_name).await?;
+    // // let task_name = "incl10-parallel-watch".to_string();
+    // let task_name = "sequential-echos-watch".to_string();
+    // commander.description(task_name.clone())?;
+    // commander.run(task_name).await?;
 
+    if let Some(sub_command) = args.sub_command {
+        match sub_command {
+            // SubCommand::Completion { shell } => {
+            //     let mut cmd = Args::command();
+            //     let bin_name = cmd.get_name().to_owned();
+            //     clap_complete::generate(shell, &mut cmd, bin_name, &mut std::io::stdout());
+            // }
+            SubCommand::Run { task_name } => {
+                commander.run(task_name).await?;
+            }
+            SubCommand::Description { task_name } => {
+                println!("{}", commander.description(task_name)?);
+            }
+        }
+    }
     Ok(())
 }
 
 #[tokio::main]
 async fn main() {
-    match cli().await {
+    let args = Args::parse();
+
+    match cli(args).await {
         Ok(_) => {}
         Err(e) => {
             eprintln!("Error: {}", e);
