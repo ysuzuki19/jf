30fbdce08cf9733b05720169b68b0bbf45725ef0
ysuzuki19 <ysuzuki.cpp@gmail.com>
2023-12-18 02:58:54 +0900

    define var for cfg file name

--- src/cfg/mod.rs
+++ src/cfg/mod.rs
@@ -4,7 +4,7 @@ use std::collections::HashMap;
 
 use serde::Deserialize;
 
-use crate::error::JfResult;
+use crate::error::{JfError, JfResult};
 
 #[derive(Debug, Deserialize)]
 pub struct Cfg {
@@ -12,9 +12,12 @@ pub struct Cfg {
     pub jobs: HashMap<String, job_cfg::JobCfg>,
 }
 
+const DEFAULT_CFG: &str = "jf.toml";
+
 impl Cfg {
     pub fn load() -> JfResult<Self> {
-        let cfg_content = std::fs::read_to_string("jf.toml")?;
+        let cfg_content = std::fs::read_to_string(DEFAULT_CFG)
+            .map_err(|_| JfError::Custom(DEFAULT_CFG.to_string()))?;
         Ok(toml::from_str(&cfg_content)?)
     }
 }
