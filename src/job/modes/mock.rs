df20278064a149c85ba1a4797257f444c268589f
ysuzuki19 <ysuzuki.cpp@gmail.com>
2024-01-02 21:50:04 +0900

    improve Mock assertion usage

--- src/job/modes/mock.rs
+++ src/job/modes/mock.rs
@@ -8,10 +8,42 @@ use crate::job::Runner;
 
 static MOCK_ID_COUNTER: AtomicUsize = AtomicUsize::new(0);
 
-pub struct MockStatus {
-    is_running: bool,
-    is_finished: bool,
-    is_cancelled: bool,
+struct CheckList {
+    is_started: Option<bool>,
+    is_running: Option<bool>,
+    is_finished: Option<bool>,
+    is_cancelled: Option<bool>,
+}
+
+impl CheckList {
+    pub fn new() -> Self {
+        Self {
+            is_started: None,
+            is_running: None,
+            is_finished: None,
+            is_cancelled: None,
+        }
+    }
+
+    pub fn started(mut self, flag: bool) -> Self {
+        self.is_started = Some(flag);
+        self
+    }
+
+    pub fn running(mut self, flag: bool) -> Self {
+        self.is_running = Some(flag);
+        self
+    }
+
+    pub fn finished(mut self, flag: bool) -> Self {
+        self.is_finished = Some(flag);
+        self
+    }
+
+    pub fn cancelled(mut self, flag: bool) -> Self {
+        self.is_cancelled = Some(flag);
+        self
+    }
 }
 
 #[derive(Debug, Clone, serde::Deserialize)]
@@ -25,6 +57,7 @@ pub struct Mock {
     each_sleep_time: u64,
     sleep_count: u8,
     id: usize,
+    is_started: Arc<AtomicBool>,
     is_running: Arc<AtomicBool>,
     is_finished: Arc<AtomicBool>,
     is_cancelled: Arc<AtomicBool>,
@@ -44,19 +77,74 @@ impl Mock {
         self.id
     }
 
-    pub fn assert_status(&self, status: MockStatus) {
-        assert_eq!(self.is_running.load(Ordering::Relaxed), status.is_running);
-        assert_eq!(self.is_finished.load(Ordering::Relaxed), status.is_finished);
+    pub fn assert_id_eq(&self, id: usize) {
         assert_eq!(
-            self.is_cancelled.load(Ordering::Relaxed),
-            status.is_cancelled
+            self.id, id,
+            "Mock({}).id is expected {} but {}",
+            self.id, id, self.id
         );
+    }
+
+    pub fn assert_id_ne(&self, id: usize) {
+        assert_ne!(self.id, id, "Mock({}).id is expected not {}", self.id, id);
+    }
+
+    pub fn assert_each_sleep_time_eq(&self, sleep_time: u64) {
+        assert_eq!(
+            self.each_sleep_time, sleep_time,
+            "Mock({}).each_sleep_time is expected {} but {}",
+            self.id, sleep_time, self.each_sleep_time
+        );
+    }
+
+    pub fn assert_sleep_count_eq(&self, sleep_count: u8) {
+        assert_eq!(
+            self.sleep_count, sleep_count,
+            "Mock({}).sleep_count is expected {} but {}",
+            self.id, sleep_count, self.sleep_count
+        );
+    }
+
+    fn assert_status(&self, check_list: CheckList) {
+        if let Some(is_started) = check_list.is_started {
+            let flag = self.is_started.load(Ordering::Relaxed);
+            assert_eq!(
+                flag, is_started,
+                "Mock({}).is_started is expected {} but {}",
+                self.id, is_started, flag
+            );
+        }
+        if let Some(is_running) = check_list.is_running {
+            let flag = self.is_running.load(Ordering::Relaxed);
+            assert_eq!(
+                flag, is_running,
+                "Mock({}).is_running is expected {} but {}",
+                self.id, is_running, flag
+            );
+        }
+        if let Some(is_finished) = check_list.is_finished {
+            let flag = self.is_finished.load(Ordering::Relaxed);
+            assert_eq!(
+                flag, is_finished,
+                "Mock({}).is_finished is expected {} but {}",
+                self.id, is_finished, flag
+            );
+        }
+        if let Some(is_cancelled) = check_list.is_cancelled {
+            let flag = self.is_cancelled.load(Ordering::Relaxed);
+            assert_eq!(
+                flag, is_cancelled,
+                "Mock({}).is_cancelled is expected {} but {}",
+                self.id, is_cancelled, flag
+            );
+        }
     }
 }
 
 #[async_trait::async_trait]
 impl Runner for Mock {
     async fn start(&self) -> JfResult<Self> {
+        self.is_started.store(true, Ordering::Relaxed);
         self.is_running.store(true, Ordering::Relaxed);
         tokio::spawn({
             let each_sleep_time = self.each_sleep_time;
@@ -103,7 +191,6 @@ impl From<Mock> for Job {
 
 mod test {
     use super::*;
-    use std::sync::atomic::Ordering;
 
     const MOCK_SLEEP_TIME: u64 = 1;
     const MOCK_SLEEP_COUNT: u8 = 3;
@@ -119,65 +206,83 @@ mod test {
     async fn new() {
         let mock = test_mock_factory();
 
-        mock.assert_status(MockStatus {
-            is_running: false,
-            is_finished: false,
-            is_cancelled: false,
-        });
+        mock.assert_status(
+            CheckList::new()
+                .started(false)
+                .running(false)
+                .finished(false)
+                .cancelled(false),
+        );
     }
 
     #[tokio::test]
-    async fn run_wait() {
+    async fn run_wait() -> JfResult<()> {
         let mock = test_mock_factory();
         let id = mock.id();
 
-        assert!(mock.start().await.is_ok());
-        mock.assert_status(MockStatus {
-            is_running: true,
-            is_finished: false,
-            is_cancelled: false,
-        });
+        mock.start().await?;
+        mock.assert_status(
+            CheckList::new()
+                .started(true)
+                .running(true)
+                .cancelled(false),
+        );
 
-        assert!(mock.wait().await.is_ok());
-        mock.assert_status(MockStatus {
-            is_running: false,
-            is_finished: true,
-            is_cancelled: false,
-        });
+        mock.wait().await?;
+        mock.assert_status(
+            CheckList::new()
+                .started(true)
+                .running(false)
+                .finished(true)
+                .cancelled(false),
+        );
 
-        assert_eq!(mock.id(), id);
+        mock.assert_id_eq(id);
+        Ok(())
     }
 
     #[tokio::test]
-    async fn run_cancel_wait() {
+    async fn run_cancel_wait() -> JfResult<()> {
         let mock = test_mock_factory();
         let id = mock.id();
 
-        assert!(mock.start().await.is_ok());
+        mock.start().await?.cancel().await?;
+        mock.assert_status(CheckList::new().cancelled(true));
 
-        assert!(mock.cancel().await.is_ok());
-        assert!(mock.is_running.load(Ordering::Relaxed));
-        assert!(!mock.is_finished.load(Ordering::Relaxed));
-        assert!(mock.is_cancelled.load(Ordering::Relaxed));
+        mock.wait().await?;
+        mock.assert_status(CheckList::new().running(false).finished(true));
 
-        assert!(mock.wait().await.is_ok());
-        assert!(!mock.is_running.load(Ordering::Relaxed));
+        mock.assert_id_eq(id);
 
-        assert_eq!(mock.id(), id);
+        Ok(())
     }
 
     #[tokio::test]
-    async fn bunshin() {
+    async fn bunshin() -> JfResult<()> {
         let mock = test_mock_factory();
         let id = mock.id();
+
+        mock.start().await?.cancel().await?;
+        mock.assert_status(
+            CheckList::new()
+                .started(true)
+                .running(true)
+                .finished(false)
+                .cancelled(true),
+        );
 
         let bunshin = mock.bunshin();
-        assert_ne!(bunshin.id(), id); // check new mock job creation
-        assert_eq!(bunshin.each_sleep_time, mock.each_sleep_time);
-        assert_eq!(bunshin.sleep_count, mock.sleep_count);
-        assert!(!bunshin.is_running.load(Ordering::Relaxed));
-        assert!(!bunshin.is_finished.load(Ordering::Relaxed));
-        assert!(!bunshin.is_cancelled.load(Ordering::Relaxed));
+        bunshin.assert_id_ne(id); // check new mock job creation
+        bunshin.assert_each_sleep_time_eq(mock.each_sleep_time);
+        bunshin.assert_sleep_count_eq(mock.sleep_count);
+        bunshin.assert_status(
+            CheckList::new()
+                .started(false)
+                .running(false)
+                .finished(false)
+                .cancelled(false),
+        );
+        Ok(())
     }
 
     #[tokio::test]
@@ -186,12 +291,16 @@ mod test {
         let id = mock.id();
 
         if let Job::Mock(mock) = mock.into() {
-            assert_eq!(mock.id(), id);
-            assert_eq!(mock.each_sleep_time, MOCK_SLEEP_TIME);
-            assert_eq!(mock.sleep_count, MOCK_SLEEP_COUNT);
-            assert!(!mock.is_running.load(Ordering::Relaxed));
-            assert!(!mock.is_finished.load(Ordering::Relaxed));
-            assert!(!mock.is_cancelled.load(Ordering::Relaxed));
+            mock.assert_id_eq(id);
+            mock.assert_each_sleep_time_eq(MOCK_SLEEP_TIME);
+            mock.assert_sleep_count_eq(MOCK_SLEEP_COUNT);
+            mock.assert_status(
+                CheckList::new()
+                    .started(false)
+                    .running(false)
+                    .finished(false)
+                    .cancelled(false),
+            );
         } else {
             panic!("Invalid Variant: Job::Mock expected");
         }
